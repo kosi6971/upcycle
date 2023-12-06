410e58b80660cd4885d753fe81c606a77de3a8e4
kosi <84139325+kosi6971@users.noreply.github.com>
2023-12-06 14:34:39 +0900

    1

--- js/Q&A.js
+++ js/Q&A.js
@@ -1,3 +1,30 @@
 $(()=>{
+    const contentBody = $(".contentBody");
+    let saveP = 0;
+
+    contentBody.on("click", function () {
+        const thisBody = $(this)
+        const thisNotBody = thisBody.siblings()
+
+        if(thisBody.attr("value") == saveP){
+            thisBody.css(
+                "height", "51px"
+            );
+        }
+        else{
+            thisBody.css({
+                "height" : "1550px",
+                "border-bottom" : "1px solid #CDD9C5;"
+            });
+        }
+
+        thisNotBody.css({
+            "height" : "51px",
+            "border-bottom" : "0px solid #CDD9C5;"
+        });
+
+        if(thisBody.attr("value") == saveP) saveP = 0;
+        else saveP = thisBody.attr("value")
+    })
     
 });
